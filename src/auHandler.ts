c1535a40a5fb3f1a486542a8b819c019febca1ad
Carlos Leon <cleon@emeals.com>
2021-09-28 00:22:51 -0400

    remove sentry from authorizer

--- src/auHandler.ts
+++ src/auHandler.ts
@@ -1,14 +1,8 @@
 import authorizerEventHandlerFactory from './authorizerEventHandlerFactory'
 import type { AuthorizerEventHandlerFactoryInputFn } from './types'
-import { AWSLambda } from '@sentry/serverless'
-import { config } from './sentry'
-
-AWSLambda.init(config)
 
 const auHandler = (
   fn: AuthorizerEventHandlerFactoryInputFn
-) => AWSLambda.wrapHandler(
-  authorizerEventHandlerFactory(fn)
-)
+) => authorizerEventHandlerFactory(fn)
 
 export default auHandler
