97664d655c506715b0b93c4e90006c6882e021d0
Carlos Leon <cleon@emeals.com>
2022-02-27 01:47:03 -0400

    bump version, async fix

--- src/agHandler.ts
+++ src/agHandler.ts
@@ -1,14 +1,15 @@
 import apiGatewayEventHandlerFactory from './apiGatewayEventHandlerFactory'
-import type { ApiGatewayEventHandlerFactoryInputFn } from './types'
+import type { ApiGatewayEventHandlerFactoryInputFn, ApiGatewayEventHandlerFactoryOptions } from './types'
 import { AWSLambda } from '@sentry/serverless'
 import { config } from './sentry'
 
 AWSLambda.init(config)
 
-const agHandler = (
-  fn: ApiGatewayEventHandlerFactoryInputFn
+const agHandler = async (
+  fn: ApiGatewayEventHandlerFactoryInputFn,
+  options?: ApiGatewayEventHandlerFactoryOptions
 ) => AWSLambda.wrapHandler(
-  apiGatewayEventHandlerFactory(fn)
+  await apiGatewayEventHandlerFactory(fn, options)
 )
 
 export default agHandler
